d4f9ba6d475ff005d84c3defe3315eae1284ca16
Nick Butts <nlbutts@ieee.org>
2016-09-10 00:12:37 +0000

    Updated.

---
+++ run_video_tl.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sleep 10
+sudo /usr/bin/python /home/pi/projects/video_tl/video_tl.py
+
--- video_tl.py
+++ video_tl.py
@@ -6,8 +6,9 @@ import os
 
 def main():
 	# Create a directory for the current time
+	os.chdir('/home/pi/projects/video_tl')
 	dir = time.strftime('%Y_%m_%d_%H_%M_%S')
-	print('Creating directory ' + dir)
+	#print('Creating directory ' + dir)
 	os.mkdir(dir)
 	os.chdir(dir)
 	
@@ -15,14 +16,14 @@ def main():
 	counter = 0;
 	while True:
 		camera.annotate_background = picamera.Color('black')
-		camera.annotate_text = time.strftime('%Y-%m-%d %H:%M:%S')
+		camera.annotate_text = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 		filename = 'img{0:06d}.jpg'.format(counter)
 		img = camera.capture(filename)
 		counter = counter + 1
-		print('Captured ' + filename)
+		#print('Captured ' + filename)
 		# Capture once an hour
-		#time.sleep(60*30)
-		time.sleep(5)
+		time.sleep(60*10)
+		#time.sleep(5)
 
 if __name__ == "__main__":
 	print('Starting Time lapse recorded')
